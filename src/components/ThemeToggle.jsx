622bf0952dfcbe9e5c40d4f04103fc7e1090ad83
Jastoni Villanueva <vjastoni13@gmail.com>
2026-02-21 12:22:49 +0800

    fixed alignments of theme toggle

--- src/components/ThemeToggle.jsx
+++ src/components/ThemeToggle.jsx
@@ -34,7 +34,7 @@ export const ThemeToggle = () => {
     <button
       onClick={toggleTheme}
       className={cn(
-        "fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transitions-colors duration-300",
+        "fixed max-sm:hidden top-1.5 right-5 z-50 p-2 rounded-full transitions-colors duration-300",
         "focus:outline-hidden",
       )}
     >
